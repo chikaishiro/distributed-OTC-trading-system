406f574aa00f08e61c831d1b269dec12c89193b2
DESKTOP-O0O9BJR\陶心旋 <592298620@sjtu.edu.cn>
2019-05-23 20:46:42 +0800

    entity

---
+++ Trader-gateway/src/main/java/com/trading/tradergateway/Entity/Broker.java
@@ -0,0 +1,74 @@
+package com.trading.tradergateway.Entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Broker")
+public class Broker {
+    @Id
+    @Column(nullable = false, name = "broker_id")
+    private String brokerID;
+
+    @Column(nullable = false, name = "broker_name")
+    private String brokerName;
+
+    @Column(name = "broker_http")
+    private String brokerHttp;
+
+    @Column(name = "broker_ws")
+    private String brokerWs;
+
+    @Column(name = "broker_token")
+    private String brokerToken;
+
+    public Broker() {}
+
+    public Broker(String brokerID, String brokerName, String brokerHttp, String brokerWs, String brokerToken) {
+        this.brokerID = brokerID;
+        this.brokerName = brokerName;
+        this.brokerHttp = brokerHttp;
+        this.brokerWs = brokerWs;
+        this.brokerToken = brokerToken;
+    }
+
+    public String getBrokerID() {
+        return brokerID;
+    }
+
+    public void setBrokerID(String brokerID) {
+        this.brokerID = brokerID;
+    }
+
+    public String getBrokerName() {
+        return brokerName;
+    }
+
+    public void setBrokerName(String brokerName) {
+        this.brokerName = brokerName;
+    }
+
+    public String getBrokerToken() {
+        return brokerToken;
+    }
+
+    public void setBrokerToken(String brokerToken) {
+        this.brokerToken = brokerToken;
+    }
+
+    public String getBrokerHttp() {
+        return brokerHttp;
+    }
+
+    public void setBrokerHttp(String brokerHttp) {
+        this.brokerHttp = brokerHttp;
+    }
+
+    public String getBrokerWs() {
+        return brokerWs;
+    }
+
+    public void setBrokerWs(String brokerWs) {
+        this.brokerWs = brokerWs;
+    }
+}
