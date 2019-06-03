64538a7486ab9e4b314ca5177154fa07a6ce2032
chikaishiro <gran_turismo@126.com>
2019-06-04 00:33:14 +0800

    resultAPI Finished

--- Broker-gateway/src/main/java/com/trading/brokergateway/Controller/WebsocketServer.java
+++ Broker-gateway/src/main/java/com/trading/brokergateway/Controller/WebsocketServer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+import com.google.gson.Gson;
+import com.trading.brokergateway.Methods.StoreUtil;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.slf4j.Logger;
@@ -20,7 +22,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-@ServerEndpoint("/websocket")
+@ServerEndpoint("/orderbook")
 @Component
 public class WebsocketServer {
     // 日志记录
@@ -32,8 +34,8 @@ public class WebsocketServer {
     // 在线session记录
     private Queue<Session> sessionQueue = new ConcurrentLinkedQueue<>();
 
-    // Redis连接
-    private Jedis jedis = new Jedis("localhost");
+
+
 
 
     public Queue<Session> getSessionQueue() {
@@ -48,14 +50,14 @@ public class WebsocketServer {
     public void onOpen(Session session) {
         sessionQueue.add(session);
         onlineCount.getAndIncrement();
-        logger.info("新连接加入，当前在线人数为：" + onlineCount);
+        System.out.println("新连接加入，当前在线人数为：" + onlineCount);
     }
 
     @OnClose
     public void onClose(Session session) {
         sessionQueue.remove(session);
         onlineCount.getAndDecrement();
-        logger.info("连接关闭，当前在线人数：" + onlineCount);
+        System.out.println("连接关闭，当前在线人数：" + onlineCount);
     }
 
     @OnError
@@ -65,85 +67,47 @@ public class WebsocketServer {
         throwable.printStackTrace();
     }
 
+    public class broadCastODB implements Runnable {
+        private String futureID;
+        private Session session;
+
+        public broadCastODB(String futureID,Session session) {
+            this.futureID = futureID;
+            this.session = session;
+        }
+
+        public void run() {
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                    Gson gson = new Gson();
+                    String i = gson.toJson(StoreUtil.GetQueue(futureID));
+                    sendMessage(session,i);
+                }
+                catch (Exception e){
+                    return;
+                }
+
+            }
+        }
+    }
     @OnMessage
     public void onMessage(final Session session, String message) {
-        //logger.info("来自客户端的消息：" + message);
-
-        String[] params = message.split(",");
-        if (params[0].equals("orderBook")) {
-            deliverFuturesMarket(session, message);
-        } else if (params[0].equals("trade")) {
-            deliverTradeHistory(session, message);
-        }
+        System.out.println("来自客户端的消息：" + message);
+        String futureID = message;
+        broadCastODB bodb = new broadCastODB(futureID,session);
+        bodb.run();
+//        String[] params = message.split(",");
+//        if (params[0].equals("orderBook")) {
+//            deliverFuturesMarket(session, message);
+//        } else if (params[0].equals("trade")) {
+//            deliverTradeHistory(session, message);
+//        }
 
     }
 
-    private void deliverTradeHistory(Session session, String message) {
-        if (!sessionQueue.contains(session)) {
-            logger.info("当前在线人数：" + onlineCount);
-            return;
-        }
-        String[] params = message.split(",");
-        String futureID = params[1];
-        String brokerName = params[2];
 
-        String key = "trade," + brokerName + "," + futureID;
-        //System.out.println("key=" + key);
-        String result = getTradeHistoryFromRedis(key);
-        sendMessage(session, result);
-        //logger.info("服务端返回: " + result);
 
-        try {
-            Thread.sleep(800);
-        } catch (Exception e) {
-            e.printStackTrace();
-            onClose(session);
-        }
-    }
-
-    private String getTradeHistoryFromRedis(String key) {
-        String trades = (String)jedis.get(key);
-        if (trades == null) return null;
-        JSONArray jsonTrades = JSONArray.fromObject(trades);
-        JSONObject jsonResult = new JSONObject();
-        jsonResult.put("type", "trade");
-        jsonResult.put("data", jsonTrades);
-
-        return jsonResult.toString();
-    }
-
-    private String getFuturesMarketFromRedis(String key) {
-        String market = jedis.get(key);
-        if (market == null) return null;
-        JSONObject jsonMarket = JSONObject.fromObject(market);
-        JSONObject jsonResult = new JSONObject();
-        jsonResult.put("type", "orderBook");
-        jsonResult.put("data", jsonMarket);
-        return jsonResult.toString();
-    }
-
-    private void deliverFuturesMarket(Session session, String message) {
-        if (!sessionQueue.contains(session)) {
-            logger.info("当前在线人数：" + onlineCount);
-            return;
-        }
-        String[] params = message.split(",");
-        String futureID = params[1];
-        String brokerName = params[2];
-
-        String key = "orderBook," + brokerName + "," + futureID;
-        //System.out.println("key=" + key);
-        String result = getFuturesMarketFromRedis(key);
-        sendMessage(session, result);
-        //logger.info("服务端返回: " + result);
-
-        try {
-            Thread.sleep(800);
-        } catch (Exception e) {
-            e.printStackTrace();
-            onClose(session);
-        }
-    }
 
     private void sendMessage(Session session, String msg) {
         try {
