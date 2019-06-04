e9dc0a4c7518c5f681197ae43c9e40a9d11a8a36
DESKTOP-O0O9BJR\陶心旋 <592298620@sjtu.edu.cn>
2019-06-04 15:33:28 +0800

    get order

--- Trader-gateway/src/main/java/com/trading/tradergateway/Controller/OrderController.java
+++ Trader-gateway/src/main/java/com/trading/tradergateway/Controller/OrderController.java
@@ -2,12 +2,14 @@ package com.trading.tradergateway.Controller;
 
 import com.trading.tradergateway.Entity.Order;
 import com.trading.tradergateway.Service.Interface.OrderService;
+import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -20,7 +22,13 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @RequestMapping(value="/SendOrder",method= RequestMethod.POST)
+    @GetMapping("/GetOrder")
+    public JSONArray getOrder(HttpServletRequest request){
+        List list = orderService.getOrder(request);
+        return JSONArray.fromObject(list);
+    }
+
+    @PostMapping("/SendOrder")
     public String sendOrder(@RequestBody Order order, HttpServletRequest request) throws Exception {
         String result = null;
         if (order.getType() == 'C'){
@@ -33,5 +41,6 @@ public class OrderController {
         }
         return result;
     }
+
 
 }
