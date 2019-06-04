2e999d6e525591c34e19569613b7e6f29fa61d18
DESKTOP-O0O9BJR\陶心旋 <592298620@sjtu.edu.cn>
2019-06-04 13:18:18 +0800

    jpa api

--- Broker-gateway/src/main/java/com/trading/brokergateway/DAO/ResultRepository.java
+++ Broker-gateway/src/main/java/com/trading/brokergateway/DAO/ResultRepository.java
@@ -4,7 +4,14 @@ import com.trading.brokergateway.Entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("ResultRepository")
 public interface ResultRepository extends JpaRepository<Result, String> {
     Result findResultByResultID(String resultID);
+    List findResultsByPriceOrderByFinishTimeDesc(double price);
+    List findResultsByOrderID1OrOrderID2OrderByFinishTimeDesc(String orderId1, String orderId2);
+    List findallOrderByFinishTimeDesc();
+    List findResultsByFutureIDOrderByFinishTime(String futureId);
+    List findResultsByFinishTimeBetweenOrderByFinishTimeDesc(long start, long end);
 }
--- Broker-gateway/src/main/java/com/trading/brokergateway/DAO/ResultService.java
+++ Broker-gateway/src/main/java/com/trading/brokergateway/DAO/ResultService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.UUID;
 
 @Service("ResultService")
@@ -20,7 +21,34 @@ public class ResultService {
         resultRepository.save(result);
     }
 
+    public static List<Result> findByPrice(double price){
+        return resultRepository.findResultsByPriceOrderByFinishTimeDesc(price).subList(0,20);
+    }
 
+    public static List<Result> findByOrderId(String orderId){
+        return resultRepository.findResultsByOrderID1OrOrderID2OrderByFinishTimeDesc(orderId, orderId).subList(0,20);
+    }
+
+    public static List<Result> findall(){
+        return resultRepository.findallOrderByFinishTimeDesc().subList(0,20);
+    }
+
+    public static List<Result> findByFutureId(String futureId){
+        return resultRepository.findResultsByFutureIDOrderByFinishTime(futureId).subList(0,20);
+    }
+
+    public static List<Result> findToday (){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0); //当天0点
+        long start = calendar.getTimeInMillis();
+        calendar.set(Calendar.HOUR_OF_DAY, 24);
+        long end =  calendar.getTimeInMillis();
+        return resultRepository.findResultsByFinishTimeBetweenOrderByFinishTimeDesc(start, end).subList(0, 20);
+
+    }
 
     public static void main(String[] args){
         Result result = new Result();
